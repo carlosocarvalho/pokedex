00d583a8f84b06c62d6effb5fdf0a16bc7c341a4
carlosocarvalho <>
2020-04-09 12:41:06 -0300

    Updates

---
+++ precache-manifest.c7e4aee25b42bfe2ac9f93f826e7d930.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea5b40ff6e2575a058508a18333b444c",
+    "url": "/pokedex.gh/index.html"
+  },
+  {
+    "revision": "dcbfdb8547083e5583f1",
+    "url": "/pokedex.gh/static/css/main.dac7ed47.chunk.css"
+  },
+  {
+    "revision": "5be9d8fb7eb65471ea35",
+    "url": "/pokedex.gh/static/js/2.75211943.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/pokedex.gh/static/js/2.75211943.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcbfdb8547083e5583f1",
+    "url": "/pokedex.gh/static/js/main.3bf839d7.chunk.js"
+  },
+  {
+    "revision": "41003087a3b46296876f",
+    "url": "/pokedex.gh/static/js/runtime-main.f3b70d74.js"
+  }
+]);
